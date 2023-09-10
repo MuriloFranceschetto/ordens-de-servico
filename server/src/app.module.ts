f4a2dbeb3db576277e04c8322938e8c92cb18e59
Murilo Franceschetto <gfmurilogf@gmail.com>
2023-09-10 13:01:22 -0300

    WIP - Criando classes  - Person

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PersonModule } from './person/person.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host: 'localhost',
+			port: 5432,
+			password: '123456',
+			username: 'postgres',
+			entities: [],
+			database: 'trampos',
+			synchronize: true,
+			logging: true,
+		}),
+		PersonModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
