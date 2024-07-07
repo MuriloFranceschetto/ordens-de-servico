28fd31fbadb9c5d5b526220211393b02e3c7f8ca
Murilo Franceschetto <gfmurilogf@gmail.com>
2024-07-07 14:02:03 +0100

    Do not require price when subservice is type REFER

--- server/src/subservice/dto/create-subservice.dto.ts
+++ server/src/subservice/dto/create-subservice.dto.ts
@@ -1,9 +1,30 @@
+import { Expose } from "class-transformer";
 import { ChargeTypes } from "../charge-type";
+import { IsBoolean, IsEnum, IsNotEmpty, IsString, ValidateIf } from "class-validator";
 
 
 export class CreateSubserviceDTO {
+
+    @Expose()
+    @IsNotEmpty({ message: 'The property active cannot be empty' })
+    @IsBoolean({ message: 'The property open must be a boolean' })
     active: boolean;
+
+    @Expose()
+    @IsNotEmpty({ message: 'The property name cannot be empty' })
+    @IsString()
     name: string;
+
+    @Expose()
+    @IsNotEmpty({ message: 'The property charged_per cannot be empty' })
+    @IsEnum(ChargeTypes)
     charged_per: ChargeTypes;
+
+    @Expose()
+    @ValidateIf((obj: CreateSubserviceDTO) => {
+        return obj.charged_per !== ChargeTypes.REFER;
+    })
+    @IsNotEmpty({ message: 'The property price cannot be empty' })
     price: number;
+
 }
